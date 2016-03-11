271c66db71c57625d7499a3b3ff3a179b4b01e20
Harlan Haskins <harlan@apple.com>
2016-03-10 18:45:55 -0800

    Refactored covered-files to instead be the common subdirectory to consider for covered files. Solidified some Markdown API. Added total to report.

--- tools/cov-compare/MarkdownWriter.hpp
+++ tools/cov-compare/MarkdownWriter.hpp
@@ -19,6 +19,12 @@
 #include "llvm/Support/FormattedStream.h"
 
 namespace covcompare {
+  namespace md {
+    std::string bold(std::string text);
+    std::string italic(std::string text);
+    std::string code(std::string text);
+    std::string link(std::string desc, std::string url);
+  }
   /// A struct that holds a 'column' of information, as represented in a table.
   struct Column {
     /// The alignment of the column,
@@ -50,14 +56,58 @@ namespace covcompare {
   
   /// A class that wraps multiple columns and outputs them as a Markdown table.
   class MarkdownWriter {
+  private:
+    void writeTable(raw_ostream &os);
+    void writeAnalysis(raw_ostream &os);
+    std::string bold(std::string text);
+    std::string code(std::string text);
   public:
+    /// The comparisons this writer will analyze.
+    std::vector<std::shared_ptr<FileComparison>> comparisons;
+    
     /// The columns this writer will write.
     std::vector<Column> columns;
     
     /// Writes this table to a stream.
     void write(llvm::raw_ostream &os);
     
-    MarkdownWriter(std::vector<Column> columns) : columns(columns) {}
+    MarkdownWriter(std::vector<std::shared_ptr<FileComparison>> comparisons)
+    : comparisons(comparisons) {
+      double oldTotal = 0.0;
+      double oldCount = 0.0;
+      double newTotal = 0.0;
+      double newCount = comparisons.size();
+      Column fnCol("Filename");
+      Column prevCol("Previous Coverage", Column::Alignment::Center);
+      Column currCol("Current Coverage", Column::Alignment::Center);
+      Column diffCol("Coverage Difference", Column::Alignment::Center);
+      for (auto &cmp : comparisons) {
+        dbgs() << "Creating column for: " << cmp->newItem->name << "\n";
+        if (auto old = cmp->oldItem) {
+          oldTotal += old->coveragePercentage();
+          oldCount += 1.0;
+        }
+        newTotal += cmp->newItem->coveragePercentage();
+        std::string oldPercentage = cmp->oldItem ?
+          formattedDouble(cmp->oldItem->coveragePercentage()) : "N/A";
+        std::string newPercentage =
+          formattedDouble(cmp->newItem->coveragePercentage());
+        newTotal += cmp->newItem->coveragePercentage();
+        fnCol.add(md::code(cmp->newItem->name));
+        prevCol.add(oldPercentage);
+        currCol.add(newPercentage);
+        diffCol.add(cmp->formattedCoverageDifference());
+      }
+      if (oldCount > 0.0) {
+        oldTotal /= oldCount;
+      }
+      newTotal /= newCount;
+      fnCol.elements.insert(fnCol.elements.begin(), "Total");
+      prevCol.elements.insert(prevCol.elements.begin(), formattedDouble(oldTotal));
+      currCol.elements.insert(currCol.elements.begin(), formattedDouble(oldTotal));
+      diffCol.elements.insert(diffCol.elements.begin(), formattedDouble(newTotal - oldTotal));
+      this->columns = { fnCol, prevCol, currCol, diffCol };
+    }
   };
 }
 
